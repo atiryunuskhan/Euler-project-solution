cff7c9e30e3c31f22713854667f8a65ceff16441
Atir Yunus Khan <48132791+atiryunuskhan@users.noreply.github.com>
2021-09-10 02:03:55 +0530

    Create Euler #2 even fibonacci numbers.java

---
+++ Euler #2 even fibonacci numbers.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++)
+        {
+            long n =in.nextLong();
+            long sum=0;
+            long n1=0;
+            long n2=1;
+            long h=n;
+            while(n2<=n)
+            {
+                long exc = n1;
+                n1 = n2;
+                n2 = n2 + exc;
+                if(n2 % 2 == 0 && n2<=n)
+                {
+                   sum = sum + n2;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
