c4da0fb8dce4fa7ea0a40e3d3815aa14a38028b3
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2021-09-11 00:47:51 +0530

    Update Euler #1 multiple of 3 or 5.java
    done changes

--- Euler #1 multiple of 3 or 5.java
+++ Euler #1 multiple of 3 or 5.java
@@ -1,4 +1,3 @@
-// Sum calculation for individual number is in O(1) time complexity
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -7,18 +6,32 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++)
+        for(int i = 0; i < t; i++)
         {
-            long n=in.nextLong();
-            long num_3=(n % 3 == 0)?(n-(n%3))/3 -1 : (n-(n%3))/3;
-            long num_5=(n % 5==0) ? (n-(n%5))/5 -1: (n-(n%5))/5;
-            long num_15=(n % 15==0) ? (n-(n%15))/15 -1 : (n-(n%15))/15;
-            long sum=0l;
-                sum=3*(num_3*(num_3+1)/2) +5*(num_5*(num_5+1)/2) - 15*(num_15*(num_15+1)/2);
-            System.out.println(sum);
+            long n = in.nextInt();
+            long s1=0;
+            long s2=0;
+            if((n-1)>=3)
+            {
+                long a=(n-1)/3;
+                s1=(a*(6+((a-1)*3)))/2;
+            }
+            if((n-1)>=5)
+            {
+                long b=(n-1)/5;
+                s2=(b*(10+((b-1)*5)))/2;
+            }
+            long s3=0;
+            if((n-1)>=15){
+                long c=(n-1)/15;
+                s3=(c*(30+((c-1)*15)))/2;
+            }
+            long s=s1+s2-s3;
+            System.out.println(s);
         }
     }
-}
+}   
